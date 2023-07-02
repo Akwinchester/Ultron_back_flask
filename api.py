e77dcd5e61fa552a8a17d351dd1f29667302a23d
AkWinchester <arsenijkiselev03@gmail.com>
2023-07-02 18:38:34 +0300

    Убрал промежуточный класс UserLogin работал над установкой значения current_user.is_authenticated

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
-from flask import request
+from flask import request, session
 from flask_restful import Api, Resource
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from models import User, db, UserLogin
+from models import User, db
 from flask_login import login_user, logout_user, current_user, login_required, login_manager, LoginManager
 
 api = Api()
@@ -13,17 +13,16 @@ class RegisterResource(Resource):
     def post(self):
         data = request.get_json()
         username = data.get('username')
-        email = data.get('email')
         password = data.get('password')
 
-        if not username or not email or not password:
+        if not username or not password:
             return {'message': 'Missing required fields'}, 400
 
         existing_user = User.query.filter_by(username=username).first()
         if existing_user:
             return {'message': 'Username already exists'}, 409
 
-        new_user = User(username=username, email=email, password=generate_password_hash(password))
+        new_user = User(username=username, password=generate_password_hash(password))
         db.session.add(new_user)
         db.session.commit()
 
@@ -45,7 +44,7 @@ class LoginResource(Resource):
 
         login_user(user)
 
-        return {'message': 'Перенаправление пользователя', 'redirect_url': '/Profile'}, 302, {'Location': '/Profile'}
+        return {'message': 'Перенаправление пользователя', 'redirect_url': '/Profile'}, 302, {'Location':'/Profile'}
 
     def get(self):
         if current_user.is_authenticated:
@@ -57,7 +56,7 @@ class LoginResource(Resource):
 
 @login_manager.user_loader
 def user_loader(user_id):
-    userlogin = UserLogin()
+    userlogin = User()
     return userlogin.get_user(user_id)
 
 
@@ -65,20 +64,21 @@ def user_loader(user_id):
 def request_loader(request):
     data = request.get_json()
     username = data.get('username')
-    user_login = UserLogin()
+    user_login = User()
     return user_login.get_user_from_username(username)
 
 
 class LogoutResource(Resource):
     @login_required
     def post(self):
+        print(current_user.is_authenticated)
         logout_user()
         return {'message': 'Logout successful'}, 200
 
 
 class DataResource(Resource):
     def get(self):
-        data = {"message": "салам"}
+        data = {"amount": 60, 'activity': 'Подтягивания', 'redirect_url': '/profile'}
         return data
 
     def post(self):
@@ -91,16 +91,37 @@ class DataResource(Resource):
 api.add_resource(RegisterResource, '/api/register')
 api.add_resource(LoginResource, '/api/login')
 api.add_resource(LogoutResource, '/api/logout')
-api.add_resource(DataResource, '/api/data')
+api.add_resource(DataResource, '/Profile')
 
 
 def initialize_app(app):
     api.init_app(app)
     login_manager.init_app(app)
 
+    # @app.after_request
+    # def add_cors_headers(response):
+    #     response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    #     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    #     response.headers['Access-Control-Allow-Methods'] = 'GET, POST'
+    #     return response
+
     @app.after_request
-    def add_cors_headers(response):
-        response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-        response.headers['Access-Control-Allow-Methods'] = 'GET, POST'
-        return response
+    def show_session(respose):
+        print('\033[92m' +
+              f'''ВЫПОЛНЯЕТСЯ ПОСЛЕ КАЖДОГО ЗАПРОСА
+            Сессия пользователя: {session}
+            current_user: {current_user.is_authenticated}'''
+              + '\033[0m')
+        return respose
+
+    @app.before_request
+    def show_session():
+        print('----------------------------------------------------------------------------------------------------------')
+        print('')
+        print('')
+        print('\033[94m' +
+              f'''ВЫПОЛНЯЕТСЯ ПЕРЕД КАЖДЫМ ЗАПРОСОМ
+            Сессия пользователя: {session}
+            current_user.is_authenticated: {current_user.is_authenticated}'''
+              + '\033[0m')
+        print()
--- models.py
+++ models.py
@@ -1,24 +1,39 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, current_user
 
 db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    # email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
 
+    def is_authenticated(self):
+        return current_user.is_authenticated()
 
-class UserLogin(UserMixin):
+
     def get_user(self, user_id):
         user = db.session.get(User, user_id)
         return user
 
-
     def get_user_from_username(self, username):
         user = db.session.query(User).filter_by(username=username).first()
         return user
+
+
+
+# class UserLogin(UserMixin):
+#     def get_user(self, user_id):
+#         user = db.session.get(User, user_id)
+#         return user
+#
+#     def get_user_from_username(self, username):
+#         user = db.session.query(User).filter_by(username=username).first()
+#         return user
+#
+#     def is_authenticated(self):
+#         return False
