a9e913eafac7eb4ac661ce40dc190dd54bf4d101
AkWinchester <arsenijkiselev03@gmail.com>
2023-08-29 14:25:52 +0300

    fix 3

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app.secret_key = 'mysecretkey123'
 
 
 app.config["SQLALCHEMY_DATABASE_URI"] = f'mysql+mysqlconnector://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
-# app.config['SESSION_COOKIE_SAMESITE'] = 'None'
-# app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+app.config['SESSION_COOKIE_SECURE'] = True
 db.init_app(app)
 app.debug = True
 
